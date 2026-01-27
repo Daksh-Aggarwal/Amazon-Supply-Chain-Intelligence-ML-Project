654ec2016c3ce818b751a08e0e2a13a432770e3f
pranavtiwariii <pranav_tiwari@yahoo.com>
2026-01-27 19:40:22 +0530

    Add Streamlit setup for delivery delay prediction app

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Delivery Delay Risk Prediction")
+
+st.title("AI-Based Delivery Delay Risk Prediction")
+st.write("streamlit deployment test successful 🚀")
 
